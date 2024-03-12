1c9ee0d4eb6c7270414085afa3a45799ff38e1d6
Juacin25 <joaquinhernande45@gmail.com>
2024-03-12 08:17:53 -0300

    agregado clase auto y esqueleto metodos

---
+++ Clases/Auto.js
@@ -0,0 +1,28 @@
+new class Auto{
+    constructor(vin,marca,modelo,kilometraje,anio,transmision,patente,consumo){
+        this.vin = vin;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.kilometraje = kilometraje;
+        this.anio = anio;
+        this.transmision = transmision;
+        this.patente = patente;
+        this.consumo = consumo;
+    }
+
+    getCostoPatente(pagos){
+
+    }
+
+    getCostoSeguro(pagos){
+
+    }
+
+    getGastoCombustible(km){
+
+    }
+
+    getCostoMensual(){
+        
+    }
+}
