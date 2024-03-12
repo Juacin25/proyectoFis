ac3c555cbe4ca7f7e189277a18481d4fa60e966e
Juacin25 <joaquinhernande45@gmail.com>
2024-03-12 08:19:45 -0300

    clase venta con esqueleto métodos

---
+++ Clases/Venta.js
@@ -0,0 +1,15 @@
+new class Venta{
+    constructor(id,auto,cliente,precio){
+        this.id = id;
+        this.auto = auto;
+        this.cliente = cliente;
+        this.precio = precio;
+    }
+
+    getImpuestos(){
+
+    }
+    getTasaescribano(){
+        
+    }
+}
